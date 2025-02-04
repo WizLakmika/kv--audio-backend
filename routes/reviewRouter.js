b0897c757f21374e913aabee87eac2bf91f726b7
WizLakmika <lakmikahewage28@gmail.com>
2025-02-05 00:33:43 +0530

    done review approva🙂l

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addReview, deleteReview, getReview }  from "../controllers/reviewController.js";
+import { addReview, approveReview, deleteReview, getReview }  from "../controllers/reviewController.js";
 
 const reviewRouter=express.Router();
 
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReview)
 reviewRouter.delete("/:email",deleteReview)
+reviewRouter.put("/approve/:email",approveReview)
 
 
 export default reviewRouter;
