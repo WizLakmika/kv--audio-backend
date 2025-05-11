cf6e2dd214ff9277a05f293ab07ba14fdb04abcf
WizLakmika <lakmikahewage28@gmail.com>
2025-05-12 00:55:46 +0530

    finish order api😋

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv";
 import reviewRouter from "./routes/reviewRouter.js";
 import inquiryRouter from "./routes/inquiryRouter.js";
 import cors from "cors";
+import orderRouter from "./routes/orderRouter.js";
 
 dotenv.config();
 
@@ -53,6 +54,7 @@ app.use("/api/users",userRouter)
 app.use("/api/products",productRouter)
 app.use("/api/reviews",reviewRouter)
 app.use("/api/inquiry",inquiryRouter)
+app.use("/api/orders",orderRouter)
 
 
 
